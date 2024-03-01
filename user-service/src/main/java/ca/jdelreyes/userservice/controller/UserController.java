fefa010b6741230ad9d469796bc45cd22e779a2f
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2024-02-29 19:00:58 -0500

    cancel use of keycloak due to lots of configuration

---
+++ user-service/src/main/java/ca/jdelreyes/userservice/controller/UserController.java
@@ -0,0 +1,9 @@
+package ca.jdelreyes.userservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+}
--- user-service/src/main/java/ca/jdelreyes/userservice/model/Role.java
+++ user-service/src/main/java/ca/jdelreyes/userservice/model/Role.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity(name = "users")
+@Entity(name = "roles")
 @Getter
 @Setter
 @Table
